0be349eca00aae25d05d301fdcf7bb549372730f
Nikita Skryabin <nekit2271@gmail.com>
2024-05-27 17:24:05 +0300

    Add fields to user model in admin panel and register user avatars tab

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,7 +1,19 @@
 from django.contrib.auth import get_user_model
 from django.contrib import admin
 
+from .models import UserAvatar
+
 
 User = get_user_model()
 
-admin.site.register(User)
+
+@admin.register(User)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+
+
+@admin.register(UserAvatar)
+class UserAvatarAdmin(admin.ModelAdmin):
+    list_display = ('user', 'avatar')
+    search_fields = ('user__username',)
