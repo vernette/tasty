0475869b0a0811b32baf8965531d184c23f6d472
Nikita Skryabin <nekit2271@gmail.com>
2024-05-31 12:56:28 +0300

    Create favorites app

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'api.apps.ApiConfig',
     'subscriptions.apps.SubscriptionsConfig',
+    'favorites.apps.FavoritesConfig',
 ]
 
 MIDDLEWARE = [
