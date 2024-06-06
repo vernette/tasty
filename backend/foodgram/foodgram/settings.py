0e99c1dd57d35f171b95e01e1620ef546936e32c
Nikita Skryabin <nekit2271@gmail.com>
2024-06-06 14:48:37 +0300

    Remove comment

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -147,7 +147,6 @@ DJOSER = {
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        # 'rest_framework.permissions.IsAuthenticated',
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
 
