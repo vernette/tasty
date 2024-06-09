8ebbc7e19ba0109cedcb75e438f8a04cdacb0e91
Nikita Skryabin <nekit2271@gmail.com>
2024-06-09 17:34:25 +0300

    Format favorites app

--- backend/foodgram/favorites/views.py
+++ backend/foodgram/favorites/views.py
@@ -1,7 +1,7 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from core.models import Recipe
 from favorites.models import Favorite
@@ -10,7 +10,10 @@ from favorites.models import Favorite
 class RecipeFavoriteAPIView(APIView):
     def post(self, request, id):
         recipe = get_object_or_404(Recipe, id=id)
-        favorite, created = Favorite.objects.get_or_create(user=request.user, recipe=recipe)
+        favorite, created = Favorite.objects.get_or_create(
+            user=request.user,
+            recipe=recipe
+        )
         if created:
             response_data = {
                 "id": recipe.id,
@@ -19,13 +22,21 @@ class RecipeFavoriteAPIView(APIView):
                 "cooking_time": recipe.cooking_time
             }
             return Response(response_data, status=status.HTTP_201_CREATED)
-        return Response({'errors': 'Рецепт уже в избранном.'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            {'errors': 'Рецепт уже в избранном.'},
+            status=status.HTTP_400_BAD_REQUEST
+        )
 
     def delete(self, request, id):
         recipe = get_object_or_404(Recipe, id=id)
-        favorite = Favorite.objects.filter(user=request.user, recipe=recipe).first()
+        favorite = Favorite.objects.filter(
+            user=request.user,
+            recipe=recipe
+        ).first()
         if favorite:
             favorite.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
-        return Response({'errors': 'Рецепта нет в избранном.'},
-                            status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            {'errors': 'Рецепта нет в избранном.'},
+            status=status.HTTP_400_BAD_REQUEST
+        )
