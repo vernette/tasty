e1ed736044110105b4cc6af46c50fbd266589e3a
Nikita Skryabin <nekit2271@gmail.com>
2024-06-09 17:41:27 +0300

    Format shopping-list app

--- backend/foodgram/shopping_list/models.py
+++ backend/foodgram/shopping_list/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
 
 from core.models import Recipe
 
@@ -8,8 +8,16 @@ User = get_user_model()
 
 
 class ShoppingCart(models.Model):
-    user = models.ForeignKey(User, related_name='shopping_cart', on_delete=models.CASCADE)
-    recipe = models.ForeignKey(Recipe, related_name='in_shopping_carts', on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User,
+        related_name='shopping_cart',
+        on_delete=models.CASCADE
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        related_name='in_shopping_carts',
+        on_delete=models.CASCADE
+    )
 
     class Meta:
         unique_together = ('user', 'recipe')
