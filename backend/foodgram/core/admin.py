d5c73de2aecb791d012459980b84ca190abb7dc7
Nikita Skryabin <nekit2271@gmail.com>
2024-06-15 14:36:24 +0300

    feat: admin panel improvement

--- backend/foodgram/core/admin.py
+++ backend/foodgram/core/admin.py
@@ -10,8 +10,30 @@ class RecipeIngredientInline(admin.TabularInline):
 
 class RecipeAdmin(admin.ModelAdmin):
     inlines = (RecipeIngredientInline,)
+    list_display = ('name', 'author_name', 'favorites_count')
+    search_fields = ('name', 'author__username')
+    list_filter = ('tags',)
+
+    def author_name(self, obj):
+        return obj.author.username
+
+    def favorites_count(self, obj):
+        return obj.favorites.count()
+
+    author_name.short_description = 'Автор'
+    favorites_count.short_description = 'Число добавлений в избранное'
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit')
+    search_fields = ('name',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name', 'slug')
 
 
 admin.site.register(Recipe, RecipeAdmin)
-admin.site.register(Ingredient)
-admin.site.register(Tag)
--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -10,7 +10,7 @@ User = get_user_model()
 @admin.register(User)
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name')
-    search_fields = ('username', 'email', 'first_name', 'last_name')
+    search_fields = ('username', 'email')
 
 
 @admin.register(UserAvatar)
